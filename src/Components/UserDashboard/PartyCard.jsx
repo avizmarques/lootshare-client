6fd01a0f2f0071c912427c9f747bd9ed4fa85d7a
avizmarques <aviz.marques@gmail.com>
2020-03-20 16:04:16 +0100

    added link to party from party list

--- src/Components/UserDashboard/PartyCard.jsx
+++ src/Components/UserDashboard/PartyCard.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function PartyCard(props) {
   const { pp, gp, ep, sp, cp } = props.party.chest;
   return (
     <div>
-      <div>{props.party.name}</div>
-      <div>
-        <div>{pp}</div>
-        <div>{gp}</div>
-        <div>{ep}</div>
-        <div>{sp}</div>
-        <div>{cp}</div>
-      </div>
+      <Link to={`/party/${props.party.id}`}>
+        <div>{props.party.name}</div>
+        <div>
+          <div>{pp}</div>
+          <div>{gp}</div>
+          <div>{ep}</div>
+          <div>{sp}</div>
+          <div>{cp}</div>
+        </div>
+      </Link>
     </div>
   );
 }
