d021f6831318a9e8e9eeef5fac822156f6dbb4e9
avizmarques <aviz.marques@gmail.com>
2020-03-20 16:03:38 +0100

    fetch single party information

--- src/store/party/actions.js
+++ src/store/party/actions.js
@@ -1,6 +1,7 @@
 import axios from "../../axios";
 
 export const FETCH_PARTIES_SUCCESS = "FETCH_PARTIES_SUCCESS";
+export const FETCH_PARTY_SUCCESS = "FETCH_PARTY_SUCCESS";
 
 const fetchPartiesSuccess = parties => ({
   type: FETCH_PARTIES_SUCCESS,
@@ -13,8 +14,24 @@ export const fetchParties = userId => async (dispatch, getState) => {
     const res = await axios.get(`user/${userId}/party`, {
       headers: { Authorization: `Bearer ${token}` }
     });
-    console.log(res);
     dispatch(fetchPartiesSuccess(res.data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const fetchPartySuccess = party => ({
+  type: FETCH_PARTY_SUCCESS,
+  payload: party
+});
+
+export const fetchParty = partyId => async (dispatch, getState) => {
+  try {
+    const token = getState().user.token;
+    const res = await axios.get(`party/${partyId}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    dispatch(fetchPartySuccess(res.data));
   } catch (err) {
     console.error(err);
   }
--- src/store/party/reducer.js
+++ src/store/party/reducer.js
@@ -1,13 +1,16 @@
-import { FETCH_PARTIES_SUCCESS } from "./actions";
+import { FETCH_PARTIES_SUCCESS, FETCH_PARTY_SUCCESS } from "./actions";
 
 const initialState = {
-  allParties: []
+  allParties: [],
+  currentParty: {}
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case FETCH_PARTIES_SUCCESS:
       return { ...state, allParties: action.payload };
+    case FETCH_PARTY_SUCCESS:
+      return { ...state, currentParty: action.payload };
     default:
       return state;
   }
