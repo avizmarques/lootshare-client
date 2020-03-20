62cdb197c3ddab9d509e4ec2a546ac121fc93755
avizmarques <aviz.marques@gmail.com>
2020-03-20 14:46:22 +0100

    setup party reducer with fetch party

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/reducer";
+import partyReducer from "./party/reducer";
 
 export default combineReducers({
-  user: userReducer
+  user: userReducer,
+  party: partyReducer
 });
