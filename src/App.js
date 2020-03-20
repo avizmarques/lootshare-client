9a560201648837283e8586724ac949771780a1c1
avizmarques <aviz.marques@gmail.com>
2020-03-20 16:03:09 +0100

    setup party dash component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 import UserDashboard from "./Components/UserDashboard";
+import PartyDashboard from "./Components/PartyDashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" component={Login} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/user/:id/dashboard" component={UserDashboard} />
+      <Route exact path="/party/:id" component={PartyDashboard} />
     </div>
   );
 }
---
+++ src/Components/PartyDashboard/index.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchParty } from "../../store/party/actions";
+import Chest from "../Chest";
+import PartyBalances from "./PartyBalances";
+
+export class PartyDashboard extends Component {
+  componentDidMount = () => {
+    this.props.fetchParty(this.props.match.params.id);
+  };
+
+  render = () => {
+    if (!this.props.party.name) {
+      return "Loading...";
+    }
+
+    const { name, chest, characters } = this.props.party;
+
+    return (
+      <div>
+        <h1>{name}</h1>
+        <Chest chest={chest} />
+        <div>
+          <h2>Party Balances</h2>
+          {characters.map(char => (
+            <PartyBalances character={char} />
+          ))}
+        </div>
+      </div>
+    );
+  };
+}
+
+const mapStateToProps = state => ({ party: state.party.currentParty });
+
+const mapDispatchToProps = { fetchParty };
+
+export default connect(mapStateToProps, mapDispatchToProps)(PartyDashboard);
