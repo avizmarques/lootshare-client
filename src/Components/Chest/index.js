b86ea9bcdfe5c1918db53b490a46d336f5210b8d
avizmarques <aviz.marques@gmail.com>
2020-03-26 15:53:53 +0100

    added basic style for chest component

--- src/Components/Chest/index.js
+++ src/Components/Chest/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./style.css";
 
 export default function Chest(props) {
   const { pp, gp, ep, sp, cp } = props.chest;
   return (
-    <div>
+    <div className="chest">
       <div>{pp}</div>
       <div>{gp}</div>
       <div>{ep}</div>
