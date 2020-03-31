ddf9266a4ff1743bbd7b214158095090b54d5c32
avizmarques <aviz.marques@gmail.com>
2020-03-31 15:26:15 +0200

    added character reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/reducer";
 import partyReducer from "./party/reducer";
+import characterReducer from "./character/reducer";
 
 export default combineReducers({
   user: userReducer,
-  party: partyReducer
+  party: partyReducer,
+  character: characterReducer
 });
